d3e501a6f6c5d9e17de2efb86c4fa0f49564df0f
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-06-18 22:59:09 +0900

    Users Microservice - welcome() 메서드   Value

---
+++ src/main/java/com/edujwt/userservice/vo/Greeting.java
@@ -0,0 +1,13 @@
+package com.edujwt.userservice.vo;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+public class Greeting {
+
+    @Value("${greeting.message}")
+    private String message;
+}

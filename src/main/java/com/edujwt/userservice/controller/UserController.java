6f80865cec3f49dafe327f1379f10814bdfd1d2e
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-06-19 00:59:39 +0900

    Users Microservice와 Spring cloud Gateway 연동 port 번호 출력

--- src/main/java/com/edujwt/userservice/controller/UserController.java
+++ src/main/java/com/edujwt/userservice/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/user-service")
 public class UserController {
     private Environment env;
     private UserService userService;
@@ -31,7 +31,7 @@ public class UserController {
 
     @GetMapping("/health_check")
     public String status() {
-        return "It's Working in User Service";
+        return String.format("It's Working in User Service on PORT %s", env.getProperty("local.server.port"));
     }
 
     @GetMapping("/welcome")

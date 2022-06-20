adfda4bc217b0a7fe748bfa4f35cceba419ff707
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-06-20 19:58:48 +0900

    Users Microservice - AUthenticationFilter 추가 접속 허가

--- src/main/java/com/edujwt/userservice/security/WebSecurity.java
+++ src/main/java/com/edujwt/userservice/security/WebSecurity.java
@@ -27,11 +27,12 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        //http.authorizeHttpRequests().antMatchers("/users/**").permitAll();
-        http.authorizeRequests().antMatchers("/**")
-                .hasIpAddress("127.0.0.1")
-                .and()
-                .addFilter(getAuthenticationFilter()); // 이 필터 통과한 데이터만
+        http.authorizeHttpRequests().antMatchers("/users/**").permitAll();
+
+//        http.authorizeRequests().antMatchers("/**")
+//                .hasIpAddress("121.138.158.249")
+//                .and()
+//                .addFilter(getAuthenticationFilter()); // 이 필터 통과한 데이터만
 
         http.headers().frameOptions().disable();
     }
